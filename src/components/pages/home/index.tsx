764a05ea65f3efbcc8771fd62ae33ad71f34da9d
Luke Rohenaz <rohenaz@gmail.com>
2024-02-06 12:09:07 -0500

    fix buttons

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -14,31 +14,37 @@ const HomePage: React.FC<HomePageProps> = async () => {
           <div className="w-full flex flex-col items-center justify-center h-full">
             <Suspense
               fallback={
-                <div className="w-96 h-96 flex items-center justify-center">
+                <div className="w-96 h-fit flex items-center justify-center">
                   <LoaderIcon />
                 </div>
               }
             >
               <SlideshowLoader />
             </Suspense>
+            <div className="divider divider-warning w-64 mx-auto text-warning/50">
+              BROWSE DEX
+            </div>
             <div className="flex mx-auto max-w-fit gap-4">
               <Link
                 href="/market/ordinals"
-                className="btn btn-lg btn-primary  font-bold mt-4"
+                className="flex flex-col btn btn-lg btn-primary  font-bold mt-4"
               >
                 Ordinals
+                <span className="font-normal text-xs text-neutral/50">Art</span>
               </Link>
               <Link
                 href="/market/bsv20"
-                className="btn btn-lg btn-primary  font-bold mt-4"
+                className="flex flex-col btn btn-lg btn-primary  font-bold mt-4"
               >
                 BSV20
+                <span className="font-normal text-xs text-neutral/50">Degen</span>
               </Link>
               <Link
                 href="/market/bsv21"
-                className="btn btn-lg btn-primary  font-bold mt-4"
+                className="flex flex-col btn btn-lg btn-primary  font-bold mt-4"
               >
                 BSV21
+                <span className="font-normal text-xs text-neutral/50">Pro</span>
               </Link>
             </div>
           </div>
