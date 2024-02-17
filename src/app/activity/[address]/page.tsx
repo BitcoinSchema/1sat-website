7aac8b589f66247ef89520521b3e4d6af2ecdb4d
Luke Rohenaz <rohenaz@gmail.com>
2024-02-17 09:52:54 -0500

    clean up address page component

--- src/app/activity/[address]/page.tsx
+++ src/app/activity/[address]/page.tsx
@@ -14,53 +14,38 @@ const AddressPage = async ({ params }: { params: { address: string } }) => {
 	const { promise } = http.customFetch<OrdUtxo[]>(historyUrl);
 	const history = await promise;
 
-  // // fetch txos
-	// const txosUrl = `${API_HOST}/api/txos/address/${params.address}/unspent`;
-	// const { promise: txosPromise } = http.customFetch<OrdUtxo[]>(txosUrl);
-	// const txos = await txosPromise;
-
-	// // unindexed bsv20 txos
-	// const bsv20txosUrl = `${API_HOST}/api/txos/address/${params.address}/unspent?bsv20=true`;
-	// const { promise: bsv20TxosPromise } =
-	// 	http.customFetch<OrdUtxo[]>(bsv20txosUrl);
-	// const bsv20Txos = await bsv20TxosPromise;
-
-	// // bsv20s
-	// const bsv20BalanceUrl = `${API_HOST}/api/bsv20/${params.address}/balance`;
-	// const { promise: bsv20BalancePromise } =
-	// 	http.customFetch<OrdUtxo[]>(bsv20BalanceUrl);
-	// const bsv20Balance = await bsv20BalancePromise;
-
 	return (
 		<div className="mx-auto">
 			<h1 className="text-3xl font-bold mb-4">Address Details</h1>
 			<h2>
 				{params.address} - {toBitcoin(balance)} BSV
 			</h2>
-      <div className="my-8 mx-auto w-fit">
-
-			<Link
-				href={`/activity/${params.address}/ordinals`}
-				className="btn btn-lg mr-2"
-        >
-				Ordinals
-			</Link>
-			<Link href={`/activity/${params.address}/bsv20`} className="btn btn-lg mr-2">
-				BSV20
-			</Link>
-			<Link href={`/activity/${params.address}/bsv21`} className="btn btn-lg">
-				BSV21
-			</Link>
-        </div>
+			<div className="my-8 mx-auto w-fit">
+				<Link
+					href={`/activity/${params.address}/ordinals`}
+					className="btn btn-lg mr-2"
+				>
+					Ordinals
+				</Link>
+				<Link
+					href={`/activity/${params.address}/bsv20`}
+					className="btn btn-lg mr-2"
+				>
+					BSV20
+				</Link>
+				<Link href={`/activity/${params.address}/bsv21`} className="btn btn-lg">
+					BSV21
+				</Link>
+			</div>
 			<div>
 				<h2 className="font-bold text-xl">History</h2>
 				<ul>
-          {history.map((txo) => (
-            <li key={txo.txid}>
-              <Link href={`/outpoint/${txo.outpoint}`}>{txo.outpoint}</Link>
-            </li>
-          ))}
-        </ul>
+					{history.map((txo) => (
+						<li key={txo.txid}>
+							<Link href={`/outpoint/${txo.outpoint}`}>{txo.outpoint}</Link>
+						</li>
+					))}
+				</ul>
 			</div>
 		</div>
 	);
