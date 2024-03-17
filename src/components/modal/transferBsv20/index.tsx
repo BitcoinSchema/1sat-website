61a973009b3d3bec0f88fe888be265c38dcff7e9
Luke Rohenaz <rohenaz@gmail.com>
2024-03-17 11:17:57 -0400

    fix show sym on transfer bsv21

--- src/components/modal/transferBsv20/index.tsx
+++ src/components/modal/transferBsv20/index.tsx
@@ -275,7 +275,7 @@ const TransferBsv20Modal: React.FC<TransferModalProps> = ({
 					<form onSubmit={submit}>
 						<div className="flex justify-between">
 							<div className="text-lg font-semibold">
-								Transfer {type} {id}
+								Transfer {type === AssetType.BSV20 ? id : sym} {type} 
 							</div>
 							<div
 								className="text-xs cursor-pointer text-[#aaa]"
