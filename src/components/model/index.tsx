204944455b6add4a18b0aa62f9f2aa7403118ac4
Luke Rohenaz <rohenaz@gmail.com>
2024-04-26 10:12:05 -0400

    add back shadow

--- src/components/model/index.tsx
+++ src/components/model/index.tsx
@@ -34,7 +34,7 @@ const Model: React.FC<ModelProps> = ({ src, size }) => {
 			poster="/model-poster.png"
 			// skybox-image="https://cdn.discordapp.com/attachments/1055908739804307477/1233415901735293008/wildsatchmo_nebula_HDRI_b2c6f00b-48aa-4aae-9ab0-097e0f66a3ed_1.png?ex=662d0392&is=662bb212&hm=99e023dc57886a8c04d025727058752006b98b0652bf052759d2d6636d3d86b1&width=2159"
 			environment-image="https://cdn.polyhaven.com/gallery/4f96c7a0db3cf7f5eb36.jpg?width=2159"
-			shadow-intensity="0"
+			shadow-intensity="1"
 			alt="3D Model - 1Sat Ordinal Preview"
 			onLoadStart={() => {
 				console.log("on model load start");
