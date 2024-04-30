7f89f7e57147b9bbbab03595d331ea273aae62b9
Luke Rohenaz <rohenaz@gmail.com>
2024-04-30 12:07:46 -0400

    fix not a token

--- src/components/pages/outpoint/token.tsx
+++ src/components/pages/outpoint/token.tsx
@@ -1,50 +1,49 @@
 import { API_HOST } from "@/constants";
-import { OrdUtxo } from "@/types/ordinals";
+import type { BSV20TXO, OrdUtxo } from "@/types/ordinals";
 import * as http from "@/utils/httpClient";
 import OutpointPage from ".";
 import { OutpointTab } from "./tabs";
 
 interface Props {
-  outpoint: string;
+	outpoint: string;
 }
 
 const OutpointToken = async ({ outpoint }: Props) => {
-  let artifact: OrdUtxo | undefined;
-  let bsv20: OrdUtxo | undefined;
-  try {
-    const url = `${API_HOST}/api/bsv20/outpoint/${outpoint}`;
-    const { promise } = http.customFetch<OrdUtxo>(url);
-    artifact = await promise;
-  } catch (e) {
-    console.log(e);
-  }
+	let artifact: BSV20TXO | undefined;
+	let inscription: OrdUtxo | undefined;
+	try {
+		const url = `${API_HOST}/api/bsv20/outpoint/${outpoint}`;
+		const { promise } = http.customFetch<BSV20TXO>(url);
+		artifact = await promise;
+	} catch (e) {
+		console.log(e);
+	}
 
-  try {
-    const url = `${API_HOST}/api/inscriptions/${outpoint}`;
-    const { promise } = http.customFetch<OrdUtxo>(url);
-    bsv20 = await promise;
-  } catch (e) {
-    console.log(e);
-  }
+	try {
+		const url = `${API_HOST}/api/inscriptions/${outpoint}`;
+		const { promise } = http.customFetch<OrdUtxo>(url);
+		inscription = await promise;
+	} catch (e) {
+		console.log(e);
+	}
 
-  const content =
-    artifact && artifact.data?.bsv20 ? (
-      <div>
-        <div>Token</div>
-      </div>
-    ) : (
-      <div>Not a token</div>
-    );
+	const content = inscription?.data?.bsv20 ? (
+		<div>
+			<div>Token</div>
+		</div>
+	) : (
+		<div>Not a token</div>
+	);
 
-  return (
-    <OutpointPage
-      artifact={artifact || bsv20!}
-      outpoint={outpoint}
-      content={content}
-      activeTab={OutpointTab.Token}
-    />
-  );
-  return;
+	return (
+		<OutpointPage
+			artifact={inscription!}
+			outpoint={outpoint}
+			content={content}
+			activeTab={OutpointTab.Token}
+		/>
+	);
+	return;
 };
 
 export default OutpointToken;
