aeea1f7e29504f3878a4d99b22f9b678c88963e7
Luke Rohenaz <rohenaz@gmail.com>
2024-05-04 15:17:44 -0400

    collection og image

---
+++ src/app/collection/opengraph-image.tsx
@@ -0,0 +1,47 @@
+import { Container } from "@/components/og/Container";
+import { Logo } from "@/components/og/Logo";
+import { getNotoSerifItalicFont } from "@/utils/font";
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+
+export const alt = "1Sat Ordinals Token Holders";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+export const contentType = "image/png";
+
+export default async function Image() {
+  const notoSerif = await getNotoSerifItalicFont();
+
+
+  return new ImageResponse(
+    (
+      <Container>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <div>Collections</div>
+        </div>
+
+        <Logo />
+      </Container>
+    ),
+    {
+      ...size,
+      fonts: [
+        {
+          name: "Noto Serif",
+          data: notoSerif,
+          style: "italic",
+          weight: 400,
+        },
+      ],
+    }
+  );
+}
