95d75551723ef0d744539bb32a2cac72fceb5a9b
Luke Rohenaz <rohenaz@gmail.com>
2024-05-06 11:33:48 -0400

    remove some logs

--- src/components/pages/outpoint/collection.tsx
+++ src/components/pages/outpoint/collection.tsx
@@ -7,71 +7,71 @@ import CollectionContent from "./collectionContent";
 import { OutpointTab } from "./tabs";
 
 interface Props {
-	outpoint: string;
+  outpoint: string;
 }
 
 const OutpointCollection = async ({ outpoint }: Props) => {
-	let artifact: OrdUtxo | undefined;
+  let artifact: OrdUtxo | undefined;
 
-	try {
-		const url = `${API_HOST}/api/inscriptions/${outpoint}`;
-		const { promise } = http.customFetch<OrdUtxo>(url);
-		artifact = await promise;
-	} catch (e) {
-		console.log(e);
-	}
+  try {
+    const url = `${API_HOST}/api/inscriptions/${outpoint}`;
+    const { promise } = http.customFetch<OrdUtxo>(url);
+    artifact = await promise;
+  } catch (e) {
+    console.log(e);
+  }
 
-	if (!artifact) {
-		return <div>Artifact not found</div>;
-	}
+  if (!artifact) {
+    return <div>Artifact not found</div>;
+  }
 
-	console.log({ artifact });
-	const isCollection =
-		artifact?.origin?.data?.map?.subType === "collection" ||
-		artifact?.origin?.data?.map?.subType === "collectionItem";
+  // console.log({ artifact });
+  const isCollection =
+    artifact?.origin?.data?.map?.subType === "collection" ||
+    artifact?.origin?.data?.map?.subType === "collectionItem";
 
-	if (!isCollection) {
-		return <div>Not a collection</div>;
-	}
+  if (!isCollection) {
+    return <div>Not a collection</div>;
+  }
 
-	// Get the Ordinal TXO
-	let collection: OrdUtxo | undefined;
-	const collectionUrl = `${API_HOST}/api/txos/${artifact.origin?.data?.map?.subTypeData.collectionId}`;
-	try {
-		const { promise: promiseCollection } =
-			http.customFetch<OrdUtxo>(collectionUrl);
-		collection = await promiseCollection;
-	} catch (e) {
-		console.error("Error fetching collection", e, collectionUrl);
-	}
+  // Get the Ordinal TXO
+  let collection: OrdUtxo | undefined;
+  const collectionUrl = `${API_HOST}/api/txos/${artifact.origin?.data?.map?.subTypeData.collectionId}`;
+  try {
+    const { promise: promiseCollection } =
+      http.customFetch<OrdUtxo>(collectionUrl);
+    collection = await promiseCollection;
+  } catch (e) {
+    console.error("Error fetching collection", e, collectionUrl);
+  }
 
-	// Get the collection stats
-	let stats: CollectionStats | undefined;
-	const collectionStatsUrl = `${API_HOST}/api/collections/${artifact.origin?.data?.map?.subTypeData.collectionId}/stats`;
-	try {
-		const { promise } =
-			http.customFetch<CollectionStats>(collectionStatsUrl);
-		stats = (await promise) || [];
-	} catch (e) {
-		console.error(e);
-	}
+  // Get the collection stats
+  let stats: CollectionStats | undefined;
+  const collectionStatsUrl = `${API_HOST}/api/collections/${artifact.origin?.data?.map?.subTypeData.collectionId}/stats`;
+  try {
+    const { promise } =
+      http.customFetch<CollectionStats>(collectionStatsUrl);
+    stats = (await promise) || [];
+  } catch (e) {
+    console.error(e);
+  }
 
-	if (!collection) {
-		return <div>Collection not found</div>;
-	}
+  if (!collection) {
+    return <div>Collection not found</div>;
+  }
 
-	const content = (
-		<CollectionContent artifact={artifact} collection={collection} />
-	);
+  const content = (
+    <CollectionContent artifact={artifact} collection={collection} />
+  );
 
-	return (
-		<OutpointPage
-			artifact={artifact}
-			outpoint={outpoint}
-			content={content}
-			activeTab={OutpointTab.Collection}
-		/>
-	);
+  return (
+    <OutpointPage
+      artifact={artifact}
+      outpoint={outpoint}
+      content={content}
+      activeTab={OutpointTab.Collection}
+    />
+  );
 };
 
 export default OutpointCollection;
