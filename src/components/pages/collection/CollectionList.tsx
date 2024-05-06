a9610506134da92d3c3a55e1668d4562d67b7fac
Luke Rohenaz <rohenaz@gmail.com>
2024-05-06 12:55:34 -0400

    improve collection loading state

--- src/components/pages/collection/CollectionList.tsx
+++ src/components/pages/collection/CollectionList.tsx
@@ -1,14 +1,15 @@
 "use client";
 
 import Artifact from "@/components/artifact";
-import { NUMBER_OF_ITEMS_PER_PAGE } from "@/constants";
+import { FetchStatus, NUMBER_OF_ITEMS_PER_PAGE } from "@/constants";
 import type { OrdUtxo } from "@/types/ordinals";
 import {
   fetchCollectionItems,
   fetchCollectionMarket,
 } from "@/utils/fetchCollectionData";
 import { useSearchParams } from "next/navigation";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { FaSpinner } from "react-icons/fa6";
 import { Tab } from "./CollectionNavigation";
 
 type CollectionListProps = {
@@ -30,11 +31,38 @@ export const CollectionList = ({ collectionId }: CollectionListProps) => {
   const [hasMoreMarket, setHasMoreMarket] = useState(true);
   const [hasMoreItems, setHasMoreItems] = useState(true);
 
-  const hasMore = isMarketTab ? hasMoreMarket : hasMoreItems;
-  const itemsToDisplay = isMarketTab ? marketItems : items;
+  const hasMore = useMemo(() => isMarketTab ? hasMoreMarket : hasMoreItems, [
+    isMarketTab,
+    hasMoreMarket,
+    hasMoreItems,
+  ]);
+
+  const itemsToDisplay = useMemo(() => isMarketTab ? marketItems : items, [
+    isMarketTab,
+    marketItems,
+    items,
+  ]);
+
+  // initialize to idle for both tabs
+  const [loading, setLoading] = useState<Map<Tab | string, FetchStatus>>(
+    new Map([
+      [Tab.Market, FetchStatus.Idle],
+      [Tab.Items, FetchStatus.Idle],
+    ])
+  );
+
+  const setLoadingStatus = useCallback((tab: string | Tab, status: FetchStatus) => {
+    const newLoading = new Map(loading);
+    newLoading.set(tab as Tab, status);
+    setLoading(newLoading);
+  }, [loading]);
 
   const loadMore = useCallback(async () => {
+
+    setLoadingStatus(tab, FetchStatus.Loading);
+
     if (isMarketTab) {
+
       const newItems =
         (await fetchCollectionMarket(collectionId, marketOffset)) ?? [];
 
@@ -49,48 +77,59 @@ export const CollectionList = ({ collectionId }: CollectionListProps) => {
       setHasMoreItems(newItems.length >= NUMBER_OF_ITEMS_PER_PAGE);
       setItemsOffset((offset) => offset + NUMBER_OF_ITEMS_PER_PAGE);
     }
-  }, [collectionId, isMarketTab, itemsOffset, marketOffset]);
+
+    setLoadingStatus(tab, FetchStatus.Success);
+  }, [collectionId, isMarketTab, itemsOffset, marketOffset, loading]);
 
   useEffect(() => {
     // initial load
     const fire = async () => {
       await loadMore();
     };
-    if (itemsOffset === 0 && !isMarketTab) {
+    if (itemsOffset === 0 && !isMarketTab && loading.get(Tab.Items) === FetchStatus.Idle) {
       fire();
     }
-    if (marketOffset === 0 && isMarketTab) {
+    if (marketOffset === 0 && isMarketTab && loading.get(Tab.Market) === FetchStatus.Idle) {
       fire();
     }
-  }, [loadMore, isMarketTab, marketOffset, itemsOffset]);
+  }, [loadMore, isMarketTab, marketOffset, itemsOffset, loading]);
 
-  return itemsToDisplay.length ? (
-    <>
-      <section className="w-full mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 md:gap-4">
-        {itemsToDisplay.map((item, idx) => {
-          return (
-            <Artifact
-              key={`${item.txid}-${item.vout}-${item.height}-${idx}`}
-              to={`/outpoint/${item.outpoint}`}
-              artifact={item}
-              size={600}
-              sizes={"100vw"}
-              priority={idx < 10}
-              showListingTag={!!item.data?.list?.price}
-            />
-          );
-        })}
-      </section>
-      {hasMore && (
-        <div className="w-full flex justify-center mt-4">
+  return (
+    <section className="w-full flex flex-col items-center justify-center">
+      {itemsToDisplay.length > 0 && (
+        <div className="w-full mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 md:gap-4">
+          {itemsToDisplay.map((item, idx) => {
+            return (
+              <Artifact
+                key={`${item.txid}-${item.vout}-${item.height}-${idx}`}
+                to={`/outpoint/${item.outpoint}`}
+                artifact={item}
+                size={600}
+                sizes={"100vw"}
+                priority={idx < 10}
+                showListingTag={!!item.data?.list?.price}
+              />
+            );
+          })}
+        </div>
+      )}
+      {loading.get(tab) !== FetchStatus.Success && (
+        <div className="mx-auto my-4">
+          <FaSpinner className="animate-spin" />
+        </div>
+      )}
+      {itemsToDisplay.length === 0 && loading.get(tab) === FetchStatus.Success && (
+        <div className="mx-auto my-4">{`No ${tab}${isMarketTab ? " items" : ""
+          } for this collection`}</div>
+      )}
+      {loading.get(tab) === FetchStatus.Success && itemsToDisplay.length > 0 && hasMore && (
+        <div className="mx-auto w-fit flex justify-center mt-4">
           <button type="button" className="btn" onClick={loadMore}>
             Load More
           </button>
         </div>
       )}
-    </>
-  ) : (
-    <div className="mx-auto my-4 text-center w-full">{`No ${tab}${isMarketTab ? " items" : ""
-      } for this collection`}</div>
+    </section>
+
   );
 };
--- src/components/pages/collection/CollectionNavigation.tsx
+++ src/components/pages/collection/CollectionNavigation.tsx
@@ -3,44 +3,43 @@
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export enum Tab {
-	Market = "market",
-	Items = "items",
+  Market = "market",
+  Items = "items",
 }
 
 export const CollectionNavigation = () => {
-	const router = useRouter();
-	const searchParams = useSearchParams();
-	const tab = searchParams.get("tab") ?? Tab.Market;
-	const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const tab = searchParams.get("tab") ?? Tab.Market;
+  const pathname = usePathname();
 
-	const navigateToTab = (tab: Tab) => {
-		const newSearchParams = new URLSearchParams(searchParams.toString());
-		newSearchParams.set("tab", tab);
+  const navigateToTab = (tab: Tab) => {
+    const newSearchParams = new URLSearchParams(searchParams.toString());
+    newSearchParams.set("tab", tab);
+    router.push(`${pathname}?${newSearchParams.toString()}`);
+  };
 
-		router.push(`${pathname}?${newSearchParams.toString()}`);
-	};
-
-	return (
-		<div
-			role="tablist"
-			className="tabs tabs-bordered max-w-[980px] mx-auto mb-12 sm:mb-16 font-mono"
-		>
-			<button
-				type="button"
-				role="tab"
-				onClick={() => navigateToTab(Tab.Market)}
-				className={`tab ${tab === Tab.Market ? "tab-active" : ""}`}
-			>
-				Market
-			</button>
-			<button
-				type="button"
-				role="tab"
-				onClick={() => navigateToTab(Tab.Items)}
-				className={`tab ${tab === Tab.Items ? "tab-active" : ""}`}
-			>
-				Items
-			</button>
-		</div>
-	);
+  return (
+    <div
+      role="tablist"
+      className="tabs tabs-bordered max-w-[980px] mx-auto mb-12 sm:mb-16 font-mono"
+    >
+      <button
+        type="button"
+        role="tab"
+        onClick={() => navigateToTab(Tab.Market)}
+        className={`tab ${tab === Tab.Market ? "tab-active" : ""}`}
+      >
+        Market
+      </button>
+      <button
+        type="button"
+        role="tab"
+        onClick={() => navigateToTab(Tab.Items)}
+        className={`tab ${tab === Tab.Items ? "tab-active" : ""}`}
+      >
+        Items
+      </button>
+    </div>
+  );
 };
