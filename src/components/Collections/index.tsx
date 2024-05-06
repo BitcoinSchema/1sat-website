b4c709fd28fd46b488b762945fa4b4f2aa7c22cb
Luke Rohenaz <rohenaz@gmail.com>
2024-05-06 10:52:20 -0400

    fix build

--- src/components/Collections/index.tsx
+++ src/components/Collections/index.tsx
@@ -6,10 +6,8 @@ import { useQuery } from "@tanstack/react-query";
 import { Noto_Serif } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
-import { useMemo } from "react";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
-import { useMediaQuery } from "usehooks-ts";
 import FeaturedCollections from "./featured";
 
 const notoSerif = Noto_Serif({
@@ -27,28 +25,10 @@ const Collections = () => {
       ),
   });
 
-  const smUp = useMediaQuery('(min-width: 640px)');
-  const mdUp = useMediaQuery('(min-width: 768px)');
-  const lgUp = useMediaQuery('(min-width: 1024px)');
-  const xlUp = useMediaQuery('(min-width: 1280px)');
-  const xxlUp = useMediaQuery('(min-width: 1536px)');
-
-  const settings = useMemo(() => {
-    return {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: xxlUp ? 4 : xlUp ? 4 : lgUp ? 4 : mdUp ? 3 : smUp ? 2 : 1,
-      slidesToScroll: xxlUp ? 4 : xlUp ? 4 : lgUp ? 4 : mdUp ? 3 : smUp ? 2 : 1,
-      autoplay: true,
-      autoplaySpeed: 3000,
-    };
-  }, [xxlUp, xlUp, lgUp, mdUp, smUp]);
-
   return (
     <>
       <h1 className={`text-2xl mb-4 ${notoSerif.className}`}>Featured Collections</h1>
-      <FeaturedCollections settings={settings} />
+      <FeaturedCollections />
       <h1 className={`text-2xl mb-4 ${notoSerif.className}`}>Current Hype</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 w-full">
         {data?.map((c) => (
