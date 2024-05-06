993f58096dd7ff6bc6d69ad399c2fc073643d9aa
Luke Rohenaz <rohenaz@gmail.com>
2024-05-06 11:44:42 -0400

    format price

--- src/components/pages/outpoint/listingContent.tsx
+++ src/components/pages/outpoint/listingContent.tsx
@@ -8,82 +8,82 @@ import type { OrdUtxo } from "@/types/ordinals";
 import { useSignal, useSignals } from "@preact/signals-react/runtime";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { toBitcoin } from "satoshi-bitcoin-ts";
 
 const ListingContent = ({ artifact }: { artifact: OrdUtxo }) => {
-	useSignals();
-	const router = useRouter();
-	const showCancelModal = useSignal(false);
-	const isOwner = artifact.owner === ordAddress.value;
-	return (
-		<div>
-			<div>Owner</div>
-			<div className="flex items-center mb-4">
-				<JDenticon
-					hashOrValue={artifact.owner}
-					className="mr-2 w-10 h-10"
-				/>
-				<div className="flex flex-col">
-					<div className="text-lg">{artifact.owner}</div>
-					<div className="text-sm text-[#aaa]">
-						{artifact.owner === ordAddress.value
-							? "You own this item"
-							: "Random Ordinal Enjoyer"}
-					</div>
-				</div>
-			</div>
-			<div>
-				{artifact.data?.list ? (
-					<div>
-						<div>Price</div>
-						<div>{artifact.data?.list?.price}</div>
-					</div>
-				) : (
-					<div>This item is not listed</div>
-				)}
-			</div>
+  useSignals();
+  const router = useRouter();
+  const showCancelModal = useSignal(false);
+  const isOwner = artifact.owner === ordAddress.value;
+  return (
+    <div>
+      <div>Owner</div>
+      <div className="flex items-center mb-4">
+        <JDenticon
+          hashOrValue={artifact.owner}
+          className="mr-2 w-10 h-10"
+        />
+        <div className="flex flex-col">
+          <div className="text-lg">{artifact.owner}</div>
+          <div className="text-sm text-[#aaa]">
+            {artifact.owner === ordAddress.value
+              ? "You own this item"
+              : "Random Ordinal Enjoyer"}
+          </div>
+        </div>
+      </div>
+      <div>
+        {artifact.data?.list ? (
+          <div>
+            <div>Price</div>
+            <div>{toBitcoin(artifact.data?.list?.price || 0)}</div>
+          </div>
+        ) : (
+          <div>This item is not listed</div>
+        )}
+      </div>
 
-			{/* // unlisted bsv20 */}
-			{isOwner && !artifact.data?.list && artifact.data?.bsv20 && (
-				<Link
-					href={`/market/${
-						artifact.data.bsv20.id ? "bsv21" : "bsv20"
-					}/${artifact.data.bsv20.id || artifact.data.bsv20.tick}`}
-				>
-					<button type="button" className="btn">
-						Sell
-					</button>
-				</Link>
-			)}
-			{/* // unlisted utxo */}
+      {/* // unlisted bsv20 */}
+      {isOwner && !artifact.data?.list && artifact.data?.bsv20 && (
+        <Link
+          href={`/market/${artifact.data.bsv20.id ? "bsv21" : "bsv20"
+            }/${artifact.data.bsv20.id || artifact.data.bsv20.tick}`}
+        >
+          <button type="button" className="btn">
+            Sell
+          </button>
+        </Link>
+      )}
+      {/* // unlisted utxo */}
 
-			{isOwner && artifact.data?.list && (
-				<button
-					disabled={!!artifact.spend && artifact.spend !== ""}
-					type="button"
-					className="btn disabled:text-gray-[#555]"
-					onClick={() => {
-						showCancelModal.value = true;
-					}}
-				>
-					Cancel
-				</button>
-			)}
-			{showCancelModal.value && (
-				<CancelListingModal
-					onClose={() => {
-						showCancelModal.value = false;
-					}}
-					onCancelled={(newOutpoint) => {
-						console.log("listing cancelled");
-						showCancelModal.value = false;
-						// we should navigate to the new inscription outpoint
-						router.push(`/outpoint/${newOutpoint}`);
-					}}
-					listing={artifact as Listing}
-				/>
-			)}
-		</div>
-	);
+      {isOwner && artifact.data?.list && (
+        <button
+          disabled={!!artifact.spend && artifact.spend !== ""}
+          type="button"
+          className="btn disabled:text-gray-[#555]"
+          onClick={() => {
+            showCancelModal.value = true;
+          }}
+        >
+          Cancel
+        </button>
+      )}
+      {showCancelModal.value && (
+        <CancelListingModal
+          onClose={() => {
+            showCancelModal.value = false;
+          }}
+          onCancelled={(newOutpoint) => {
+            console.log("listing cancelled");
+            showCancelModal.value = false;
+            // we should navigate to the new inscription outpoint
+            router.push(`/outpoint/${newOutpoint}`);
+          }}
+          listing={artifact as Listing}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ListingContent;
