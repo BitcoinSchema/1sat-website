8fbcbaa31b8e2a82522fe043a5fee70efd7383fa
Luke Rohenaz <rohenaz@gmail.com>
2024-05-10 14:24:48 -0400

    fix address pagte styles

--- src/app/activity/[address]/[type]/page.tsx
+++ src/app/activity/[address]/[type]/page.tsx
@@ -3,50 +3,60 @@ import WalletHistory from "@/components/Wallet/history";
 import WalletOrdinals from "@/components/Wallet/ordinals";
 import { WalletTab } from "@/components/Wallet/tabs";
 import { getCapitalizedAssetType } from "@/utils/assetType";
+import { Noto_Serif } from "next/font/google";
+
+const notoSerif = Noto_Serif({
+  style: "italic",
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
 
 const AddressPage = ({
-	params,
+  params,
 }: {
-	params: { address: string; type: WalletTab };
+  params: { address: string; type: WalletTab };
 }) => {
-	return (
-		<div className="mx-auto">
-			<h1 className="text-3xl font-bold mb-4">
-				Address: {params.address}
-			</h1>
-			{params.type === WalletTab.BSV20 ||
-			params.type === WalletTab.BSV21 ? (
-				<WalletBsv20 address={params.address} type={params.type} />
-			) : params.type === WalletTab.Ordinals ? (
-				<WalletOrdinals address={params.address} />
-			) : (
-				<WalletHistory address={params.address} />
-			)}
-		</div>
-	);
+  return (
+    <div className="mx-auto w-full max-w-7xl">
+      <h1 className={`text-lg md:text-3xl font-bold md:mb-4 ${notoSerif.className}`}>
+        Address
+      </h1>
+      <h2 className="text-sm md:text-xl font-semibold mb-4 md:mb-8">
+        {params.address}
+      </h2>
+      {params.type === WalletTab.BSV20 ||
+        params.type === WalletTab.BSV21 ? (
+        <WalletBsv20 address={params.address} type={params.type} />
+      ) : params.type === WalletTab.Ordinals ? (
+        <WalletOrdinals address={params.address} />
+      ) : (
+        <WalletHistory address={params.address} />
+      )}
+    </div>
+  );
 };
 
 export default AddressPage;
 
 export async function generateMetadata({
-	params,
+  params,
 }: {
-	params: { address: string; type: WalletTab };
+  params: { address: string; type: WalletTab };
 }) {
-	const { address, type } = params;
-	const assetType = getCapitalizedAssetType(type);
+  const { address, type } = params;
+  const assetType = getCapitalizedAssetType(type);
 
-	return {
-		title: `${assetType} activity for address ${address}`,
-		description: `View the ${assetType} activity for the address ${address} on 1SatOrdinals.`,
-		openGraph: {
-			title: `${assetType} activity for address ${address}`,
-			description: `View the ${assetType} activity for the address ${address} on 1SatOrdinals.`,
-		},
-		twitter: {
-			card: "summary_large_image",
-			title: `${assetType} activity for address ${address}`,
-			description: `View the ${assetType} activity for the address ${address} on 1SatOrdinals.`,
-		},
-	};
+  return {
+    title: `${assetType} activity for address ${address}`,
+    description: `View the ${assetType} activity for the address ${address} on 1SatOrdinals.`,
+    openGraph: {
+      title: `${assetType} activity for address ${address}`,
+      description: `View the ${assetType} activity for the address ${address} on 1SatOrdinals.`,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `${assetType} activity for address ${address}`,
+      description: `View the ${assetType} activity for the address ${address} on 1SatOrdinals.`,
+    },
+  };
 }
