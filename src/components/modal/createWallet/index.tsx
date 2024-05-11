49680e631b5fcc2889c1fa1f5c802ef9b594fda9
Luke Rohenaz <rohenaz@gmail.com>
2024-05-11 06:50:12 -0400

    fix cancel on generate wallet

--- src/components/modal/createWallet/index.tsx
+++ src/components/modal/createWallet/index.tsx
@@ -12,70 +12,69 @@ import { VerifyMnemonicStep } from "./steps/VerifyMnemonicStep";
 import { ViewMnemonicStep } from "./steps/ViewMnemonicStep";
 
 const CreateWalletModal = ({
-	open,
-	close,
+  open,
+  close,
 }: {
-	open: boolean;
-	close: (signOut?: boolean) => void;
+  open: boolean;
+  close: (signOut?: boolean) => void;
 }) => {
-	useSignals();
+  useSignals();
 
-	const alreadyHasKey = useMemo(() => !!payPk.value, []);
+  const alreadyHasKey = useMemo(() => !!payPk.value, []);
 
-	if (alreadyHasKey) {
-		console.log({ payPk: payPk.value });
-		return (
-			<div>
-				You already have a wallet! If you really want to make a new
-				wallet, sign out first
-			</div>
-		);
-	}
+  if (alreadyHasKey) {
+    return (
+      <div>
+        You already have a wallet! If you really want to make a new
+        wallet, sign out first.
+      </div>
+    );
+  }
 
-	return (
-		<dialog
-			id="create_wallet_modal"
-			className={`modal backdrop-blur	${open ? "modal-open" : ""}`}
-		>
-			<div className="modal-box">
-				<h3 className="font-bold text-lg">Create New Wallet</h3>
+  return (
+    <dialog
+      id="create_wallet_modal"
+      className={`modal backdrop-blur	${open ? "modal-open" : ""}`}
+    >
+      <div className="modal-box">
+        <h3 className="font-bold text-lg">Create New Wallet</h3>
 
-				{!bsvWasmReady.value && (
-					<div className="py-2 rounded my-2">Loading...</div>
-				)}
+        {!bsvWasmReady.value && (
+          <div className="py-2 rounded my-2">Loading...</div>
+        )}
 
-				{bsvWasmReady.value && (
-					<>
-						{createWalletStep.value === CreateWalletStep.Create && (
-							<CreateStep />
-						)}
+        {bsvWasmReady.value && (
+          <>
+            {createWalletStep.value === CreateWalletStep.Create && (
+              <CreateStep onClose={close} />
+            )}
 
-						{createWalletStep.value ===
-							CreateWalletStep.Created && <CreatedStep />}
+            {createWalletStep.value ===
+              CreateWalletStep.Created && <CreatedStep />}
 
-						{createWalletStep.value ===
-							CreateWalletStep.EnterPassphrase && (
-							<EnterPassphraseStep />
-						)}
+            {createWalletStep.value ===
+              CreateWalletStep.EnterPassphrase && (
+                <EnterPassphraseStep />
+              )}
 
-						{createWalletStep.value ===
-							CreateWalletStep.ViewMnemonic && (
-							<ViewMnemonicStep />
-						)}
+            {createWalletStep.value ===
+              CreateWalletStep.ViewMnemonic && (
+                <ViewMnemonicStep />
+              )}
 
-						{createWalletStep.value ===
-							CreateWalletStep.VerifyMnemonic && (
-							<VerifyMnemonicStep />
-						)}
+            {createWalletStep.value ===
+              CreateWalletStep.VerifyMnemonic && (
+                <VerifyMnemonicStep />
+              )}
 
-						{createWalletStep.value === CreateWalletStep.Fund && (
-							<FundStep onClose={close} />
-						)}
-					</>
-				)}
-			</div>
-		</dialog>
-	);
+            {createWalletStep.value === CreateWalletStep.Fund && (
+              <FundStep onClose={close} />
+            )}
+          </>
+        )}
+      </div>
+    </dialog>
+  );
 };
 
 export default CreateWalletModal;
