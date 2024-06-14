af0a1bc25a0cc97c3c605ae8627580badf4f99ac
Satchmo <rohenaz@gmail.com>
2024-06-14 12:55:08 -0400

    revert to woc txos

--- src/utils/address.ts
+++ src/utils/address.ts
@@ -40,25 +40,21 @@ export const getOrdUtxos = async (address: string, nextOffset: number) => {
 
 export const getUtxos = async (address: string) => {
   const { promise } = http.customFetch<OrdUtxo[]>(
-    // `https://api.whatsonchain.com/v1/bsv/main/address/${address}/unspent`
-    `https://ordinals.gorillapool.io/api/txos/address/${address}/unspent?bsv20=false`
+    `https://api.whatsonchain.com/v1/bsv/main/address/${address}/unspent`
+    // `https://ordinals.gorillapool.io/api/txos/address/${address}/unspent?bsv20=false`
   );
   const u = await promise;
 
   debugger
-  // return u.map((u: WocUtxo) => {
-  //   return {
-  //     satoshis: u.value,
-  //     txid: u.tx_hash,
-  //     vout: u.tx_pos,
-  //     script: P2PKHAddress.from_string(address)
-  //       .get_locking_script()
-  //       .to_asm_string(),
-  //   };
-  // });
-  return u.map((u) => {
-    u.script = P2PKHAddress.from_string(address).get_locking_script().to_asm_string();
-    return u;
+  return u.map((u: WocUtxo) => {
+    return {
+      satoshis: u.value,
+      txid: u.tx_hash,
+      vout: u.tx_pos,
+      script: P2PKHAddress.from_string(address)
+        .get_locking_script()
+        .to_asm_string(),
+    };
   });
 };
 
