ac637cf6a3c497d9a361357766592f6ff66ffb5f
Petar Slovic <petar@vxpass.com>
2024-07-16 11:21:15 +0200

    Fix whitespace formatting

--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -31,24 +31,20 @@ export const backupKeys = () => {
 		ordPkDerivationPath = `m/${ordAddressPath.value}`;
 	}
 
-	const identityDerivationPath =
-		!identityAddressPath.value ||
-		typeof identityAddressPath.value === "number"
-			? payPkDerivationPath
-			: identityAddressPath.value;
+	const identityDerivationPath = !identityAddressPath.value || typeof identityAddressPath.value === "number"
+		? payPkDerivationPath
+		: identityAddressPath.value;
 
 	const dataStr = `data:text/json;charset=utf-8,${encodeURIComponent(
-		JSON.stringify(
-			removeNullKeys({
-				mnemonic: mnemonic.value,
-				payPk: payPk.value,
-				payDerivationPath: payPkDerivationPath,
-				ordPk: ordPk.value,
-				ordDerivationPath: ordPkDerivationPath,
-				identityPk: identityPk.value ?? payPk.value,
-				identityDerivationPath: identityDerivationPath,
-			})
-		)
+		JSON.stringify(removeNullKeys({
+			mnemonic: mnemonic.value,
+			payPk: payPk.value,
+			payDerivationPath: payPkDerivationPath,
+			ordPk: ordPk.value,
+			ordDerivationPath: ordPkDerivationPath,
+			identityPk: identityPk.value ?? payPk.value,
+			identityDerivationPath: identityDerivationPath,
+		}))
 	)}`;
 
 	const clicker = document.createElement("a");
