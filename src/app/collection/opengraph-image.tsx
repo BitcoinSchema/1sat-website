f54c3c291eb71dec32293802a71c6c38fe50591a
Satchmo <rohenaz@gmail.com>
2024-08-07 13:30:15 -0400

    collections page meta

--- src/app/collection/opengraph-image.tsx
+++ src/app/collection/opengraph-image.tsx
@@ -24,7 +24,7 @@ export default async function Image() {
             alignItems: "center",
           }}
         >
-          <div>Collections</div>
+          <div>1Sat Market Collections</div>
         </div>
         <Logo />
       </Container>
--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -8,3 +8,21 @@ const Collection = async () => {
   );
 };
 export default Collection;
+
+export async function generateMetadata() {
+
+	return {
+		title: "1Sat Ordinals Market Collections",
+		description: "Native Bitcoin SV decentralized marketplace directly on Layer 1. The best place to find 1Sat NFTs, and Fungible tokens with way more utility than your average blockchain! Mint your own tokens, or buy and sell on the marketplace.",
+		openGraph: {
+			title: "1Sat Ordinals marketplace collections",
+			description: "Explore 1Sat Ordinals NFT Collections on the best on-chain marketplace.",
+			type: "website",
+		},
+		twitter: {
+			card: "summary_large_image",
+			title: "1Sat Ordinals marketplace Collections",
+			description: "Explore 1Sat Ordinals NFT Collections on the best on-chain marketplace.",
+		},
+	};
+}
