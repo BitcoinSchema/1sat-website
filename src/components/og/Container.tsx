3d01f9090f4b25d9147da07eb082354577740e52
Luke Rohenaz <rohenaz@gmail.com>
2024-08-06 22:03:02 -0400

    fix opengraph container

--- src/components/og/Container.tsx
+++ src/components/og/Container.tsx
@@ -1,5 +1,6 @@
 import type { PropsWithChildren } from "react";
 
+
 export function Container({ children }: PropsWithChildren) {
   return (
     <div
@@ -7,6 +8,8 @@ export function Container({ children }: PropsWithChildren) {
         fontSize: 48,
         background: "black",
         color: "white",
+        width: "100%",
+        height: "100%",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
