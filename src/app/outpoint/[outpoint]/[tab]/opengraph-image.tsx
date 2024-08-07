9bcaa12a31517a3c6197c4fcf83ba43ff06314bc
Luke Rohenaz <rohenaz@gmail.com>
2024-08-06 22:36:23 -0400

    better og images on outpoints

--- src/app/outpoint/[outpoint]/[tab]/opengraph-image.tsx
+++ src/app/outpoint/[outpoint]/[tab]/opengraph-image.tsx
@@ -1,7 +1,7 @@
 import { Container } from "@/components/og/Container";
 import { Logo } from "@/components/og/Logo";
 import { API_HOST, ORDFS } from "@/constants";
-import type { OrdUtxo } from "@/types/ordinals";
+import type { OrdUtxo, SigilMeta } from "@/types/ordinals";
 import { getNotoSerifItalicFont } from "@/utils/font";
 import { ImageResponse } from "next/og";
 
@@ -21,11 +21,6 @@ export default async function Image({
 }) {
   const notoSerif = await getNotoSerifItalicFont();
 
-  // const url = `https://res.cloudinary.com/tonicpow/image/fetch/c_crop,b_rgb:111111,g_center,h_${size.height},w_${size.width}/f_auto/${ORDFS}/${params.outpoint}`;
-  // use this instead https://res.cloudinary.com/tonicpow/image/fetch/c_fill,h_630,w_1200,b_rgb:111111/https://ordfs.network/f0542a36cfd31fdcd6bd4667b79e22741c361283f359e89d7b1a053ec28b2c24_0
-  const url = `https://res.cloudinary.com/tonicpow/image/fetch/c_fill,h_${size.height},w_${size.width},b_rgb:111111/${ORDFS}/${params.outpoint}`
-  console.log("Opengraph image url:", url)
-
   const detailsUrl = `${API_HOST}/api/inscriptions/${params.outpoint}`
   const details = await fetch(
     detailsUrl,
@@ -36,38 +31,68 @@ export default async function Image({
     return res.json() as Promise<OrdUtxo>
   });
 
+  const sigilData = details.origin?.data?.map?.sigil;
+  let sigilImageTxid: string | undefined;
+  if (sigilData) {
+    const sigil = JSON.parse(sigilData) as SigilMeta;
+    sigilImageTxid = sigil.image.split("b://")[1];
+  }
+
+  // const url = `https://res.cloudinary.com/tonicpow/image/fetch/c_crop,b_rgb:111111,g_center,h_${size.height},w_${size.width}/f_auto/${ORDFS}/${params.outpoint}`;
+  // use this instead https://res.cloudinary.com/tonicpow/image/fetch/c_fill,h_630,w_1200,b_rgb:111111/https://ordfs.network/f0542a36cfd31fdcd6bd4667b79e22741c361283f359e89d7b1a053ec28b2c24_0
+  const url = `https://res.cloudinary.com/tonicpow/image/fetch/c_fill,h_${size.height},w_${size.width},b_rgb:111111/${ORDFS}/${sigilImageTxid || params.outpoint}`
+  console.log("Opengraph image url:", url)
+
+
   const isImageInscription =
     details?.origin?.data?.insc?.file.type?.startsWith("image");
   // const url = `${ORDFS}/${params.outpoint}`;
 
+  const name = displayName(details);
   return new ImageResponse(
-    <Container>
-      {isImageInscription ? (
-        // biome-ignore lint/a11y/useAltText: <explanation>
-        // eslint-disable-next-line @next/next/no-img-element
-        <img src={url} alt={alt} title={alt} {...size} />
-      ) : (
-        details.origin?.data?.map?.name ||
-        details.origin?.data?.bsv20?.tick ||
-        details.origin?.data?.bsv20?.sym ||
-        details.origin?.data?.insc?.json?.tick ||
-        details.origin?.data?.insc?.json?.p ||
-        details.origin?.data?.insc?.file.type ||
-        "Mystery Outpoint"
-      )}
-
-      <Logo />
-    </Container>,
+    (
+      <Container>
+        {isImageInscription || sigilData ? (
+          // biome-ignore lint/a11y/useAltText: <explanation>
+          // eslint-disable-next-line @next/next/no-img-element
+          <img src={url} alt={alt} {...size} />
+        ) : (
+          name || "Mystery Outpoint"
+        )}
+        {sigilData && (
+          <div style={{
+            fontFamily: "Noto Serif",
+            fontStyle: "italic",
+            fontWeight: 400,
+            fontSize: "1.5rem",
+            top: 0,
+            left: 0,
+            margin: "1rem",
+            position: "absolute",
+          }}>{name || ""}</div>
+        )}
+        <Logo />
+      </Container>
+    ),
     {
       ...size,
       fonts: [
         {
           name: "Noto Serif",
-          data: await notoSerif,
+          data: notoSerif,
           style: "italic",
           weight: 400,
         },
       ],
-    },
+    }
   );
+}
+
+const displayName = (details: OrdUtxo): string | undefined => {
+  return details.origin?.data?.map?.name ||
+    details.origin?.data?.bsv20?.tick ||
+    details.origin?.data?.bsv20?.sym ||
+    details.origin?.data?.insc?.json?.tick ||
+    details.origin?.data?.insc?.json?.p ||
+    details.origin?.data?.insc?.file.type;
 }
--- src/app/outpoint/[outpoint]/opengraph-image.tsx
+++ src/app/outpoint/[outpoint]/opengraph-image.tsx
@@ -1,7 +1,7 @@
 import { Container } from "@/components/og/Container";
 import { Logo } from "@/components/og/Logo";
 import { API_HOST, ORDFS } from "@/constants";
-import type { OrdUtxo } from "@/types/ordinals";
+import type { OrdUtxo, SigilMeta } from "@/types/ordinals";
 import { getNotoSerifItalicFont } from "@/utils/font";
 import { ImageResponse } from "next/og";
 
@@ -13,6 +13,10 @@ export const size = {
   height: 630,
 };
 export const contentType = "image/png";
+
+
+// {"rank":1113,"image":"b://cbacb16c3a03729165542f20404827f1ee91bc1f9783089c41c59524ebf75a22","score":"6.81","title":"Rare Sirloins #2149","number":2149,"rarity":"Common","series":2180,"attributes":[{"count":36,"value":"Blue","rarity":"Epic","trait_type":"Meat"},{"count":117,"value":"Free Range Egg","rarity":"Common","trait_type":"Garnish"},{"count":154,"value":"Pees","rarity":"Common","trait_type":"Side 1"},{"count":89,"value":"Banana","rarity":"Common","trait_type":"Side 2"},{"count":54,"value":"Fresh Water","rarity":"Common","trait_type":"Background"}]}
+
 
 export default async function Image({
   params,
@@ -27,24 +31,39 @@ export default async function Image({
 
   const isImageInscription =
     details.origin?.data?.insc?.file.type?.startsWith("image");
-  // const url = `${ORDFS}/${params.outpoint}`;
-  const url = `https://res.cloudinary.com/tonicpow/image/fetch/c_fill,h_${size.height},w_${size.width},b_rgb:111111/${ORDFS}/${params.outpoint}`
 
+  const sigilData = details.origin?.data?.map?.sigil;
+  let sigilImageTxid: string | undefined;
+  if (sigilData) {
+    const sigil = JSON.parse(sigilData) as SigilMeta;
+    sigilImageTxid = sigil.image.split("b://")[1];
+  }
+  // const url = `${ORDFS}/${params.outpoint}`;
+  const url = `https://res.cloudinary.com/tonicpow/image/fetch/c_fill,h_${size.height},w_${size.width},b_rgb:111111/${ORDFS}/${sigilImageTxid || params.outpoint}`
+
+  const name = displayName(details);
   return new ImageResponse(
     (
       <Container>
-        {isImageInscription ? (
+        {isImageInscription || sigilData ? (
+          // biome-ignore lint/a11y/useAltText: <explanation>
+          // eslint-disable-next-line @next/next/no-img-element
           <img src={url} alt={alt} {...size} />
         ) : (
-          details.origin?.data?.map?.name ||
-          details.origin?.data?.bsv20?.tick ||
-          details.origin?.data?.bsv20?.sym ||
-          details.origin?.data?.insc?.json?.tick ||
-          details.origin?.data?.insc?.json?.p ||
-          details.origin?.data?.insc?.file.type ||
-          "Mystery Outpoint"
+          name || "Mystery Outpoint"
         )}
-
+        {sigilData && (
+          <div style={{
+            fontFamily: "Noto Serif",
+            fontStyle: "italic",
+            fontWeight: 400,
+            fontSize: "1.5rem",
+            top: 0,
+            left: 0,
+            margin: "1rem",
+            position: "absolute",
+          }}>{name || ""}</div>
+        )}
         <Logo />
       </Container>
     ),
@@ -53,11 +72,20 @@ export default async function Image({
       fonts: [
         {
           name: "Noto Serif",
-          data: await notoSerif,
+          data: notoSerif,
           style: "italic",
           weight: 400,
         },
       ],
     }
   );
+}
+
+const displayName = (details: OrdUtxo): string | undefined => {
+  return details.origin?.data?.map?.name ||
+    details.origin?.data?.bsv20?.tick ||
+    details.origin?.data?.bsv20?.sym ||
+    details.origin?.data?.insc?.json?.tick ||
+    details.origin?.data?.insc?.json?.p ||
+    details.origin?.data?.insc?.file.type;
 }
