24e9f9ba2b55f91322ecb23a80d00605531b6dbb
Satchmo <rohenaz@gmail.com>
2024-08-12 10:46:42 -0400

    bump js-1sat-ord, fix bsv21 inscribe form

--- src/components/pages/mine/index.tsx
+++ src/components/pages/mine/index.tsx
@@ -22,7 +22,7 @@ const MinePage: React.FC = async () => {
 						{bloodyPow}
 					</pre>
 					<div className="my-12">
-						<img
+						<Image
 							src={src}
 							height={21}
 							width={716}
