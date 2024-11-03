c773981478add40be38edd78a8d15872570d99d1
Luke Rohenaz <rohenaz@gmail.com>
2024-11-03 18:02:55 -0500

    add image cropper to bsv21 minter

--- src/components/modal/deposit/index.tsx
+++ src/components/modal/deposit/index.tsx
@@ -1,31 +1,33 @@
 import Deposit from "@/components/Wallet/deposit";
 
 interface DespotModalProps {
-	onClose: () => void;
+  onClose: () => void;
 }
 
 const DepositModal: React.FC<DespotModalProps> = ({ onClose }) => {
-	return (
-		<div
-			className="modal modal-backdrop backdrop-blur modal-open"
-			onClick={() => onClose()}
-		>
-			<div
-				className="w-full max-w-lg m-auto p-4 bg-[#111] text-[#aaa] rounded flex flex-col"
-				onClick={(e) => e.stopPropagation()}
-			>
-				<div className="relative w-full h-full overflow-hidden mb-4">
-					<Deposit />
-				</div>
+  return (
+    // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+    <div
+      className="modal modal-backdrop backdrop-blur modal-open"
+      onClick={() => onClose()}
+    >
+      {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+      <div
+        className="w-full max-w-lg m-auto p-4 bg-[#111] text-[#aaa] rounded flex flex-col"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="relative w-full h-full overflow-hidden mb-4">
+          <Deposit />
+        </div>
 
-				<form onSubmit={onClose}>
-					<div className="flex justify-end">
-						<button className="btn">Done</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	);
+        <form onSubmit={onClose}>
+          <div className="flex justify-end">
+            <button type="button" className="btn">Done</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default DepositModal;
--- src/components/pages/inscribe/bsv21.tsx
+++ src/components/pages/inscribe/bsv21.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Artifact from "@/components/artifact";
-import { FetchStatus, toastErrorProps } from "@/constants";
+import { FetchStatus } from "@/constants";
 import {
   chainInfo,
   indexers,
@@ -13,6 +13,7 @@ import type { TxoData } from "@/types/ordinals";
 import type { PendingTransaction } from "@/types/preview";
 import { getUtxos } from "@/utils/address";
 import { calculateIndexingFee } from "@/utils/bsv20";
+import { getCroppedImg } from "@/utils/canvasUtils";
 import { useIDBStorage } from "@/utils/storage";
 import { PrivateKey } from "@bsv/sdk";
 import { computed } from "@preact/signals-react";
@@ -25,9 +26,10 @@ import {
   type Utxo,
   deployBsv21Token
 } from "js-1sat-ord";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import type React from "react";
 import { useCallback, useEffect, useMemo, useState } from "react";
+import Cropper from 'react-easy-crop';
 import toast from "react-hot-toast";
 import { IoMdWarning } from "react-icons/io";
 import { RiSettings2Fill } from "react-icons/ri";
@@ -41,9 +43,23 @@ interface InscribeBsv21Props {
   inscribedCallback: () => void;
 }
 
+type CropData = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
+
+type CroppedArea = {
+  width: number;
+  height: number;
+  x: number;
+  y: number;
+};
+
 const InscribeBsv21: React.FC<InscribeBsv21Props> = ({ inscribedCallback }) => {
   useSignals();
-
+  const router = useRouter();
   const [pendingTxs, setPendingTxs] = useIDBStorage<PendingTransaction[]>(
     "1sat-pts",
     [],
@@ -53,9 +69,8 @@ const InscribeBsv21: React.FC<InscribeBsv21Props> = ({ inscribedCallback }) => {
   const tab = params.get("tab") as InscriptionTab;
   const tick = params.get("tick");
   const op = params.get("op");
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [isImage, setIsImage] = useState<boolean>(false);
-  const [preview, setPreview] = useState<string | ArrayBuffer | null>(null);
+
 
   const [fetchTickerStatus, setFetchTickerStatus] = useState<FetchStatus>(
     FetchStatus.Idle,
@@ -72,6 +87,14 @@ const InscribeBsv21: React.FC<InscribeBsv21Props> = ({ inscribedCallback }) => {
   const [iterations, setIterations] = useState<number>(1);
 
   const [ticker, setTicker] = useState<string | null>(tick);
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [zoom, setZoom] = useState(1);
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState<CroppedArea | null>(null);
+  const [croppedImage, setCroppedImage] = useState<string | null>(null);
+
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  const [selectedImageURL, setSelectedImageURL] = useState<string | null>(null);
+  const [showCropper, setShowCropper] = useState<boolean>(false);
 
   useEffect(() => {
     if (tick) {
@@ -96,61 +119,13 @@ const InscribeBsv21: React.FC<InscribeBsv21Props> = ({ inscribedCallback }) => {
     );
   });
 
-  const changeFile = useCallback(async (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files || !e.target.files[0]) {
-      return;
+  const artifact = useMemo(() => {
+    console.log({ croppedImage })
+    if (!croppedImage || typeof croppedImage !== "string") {
+      return null;
     }
-    // TODO: This reads the file twice which is pretty inefficient
-    // would be nice to get dimensions and ArrayBuffer for preview in one go
-
-    const file = e.target.files[0] as File;
-    // make sure the width and height are identical
-    const img = new Image();
-    img.onload = () => {
-      if (img.width !== img.height) {
-        toast.error("Image must be square", toastErrorProps);
-        setSelectedFile(null);
-        setPreview(null);
-        setIsImage(false);
-        setMintError("Image must be square");
-        return;
-      }
-      // max size is 400px
-      if (img.width > 400) {
-        toast.error("Width must be 400px or less", toastErrorProps);
-        setSelectedFile(null);
-        setPreview(null);
-        setIsImage(false);
-        setMintError("Width must be 400px or less");
-        return;
-      }
-      if (file.size > 100000) {
-        toast.error("Image must be less than 100KB", toastErrorProps);
-        setSelectedFile(null);
-        setPreview(null);
-        setIsImage(false);
-        setMintError("Image must be less than 100KB");
-        return;
-      }
-      setMintError(undefined);
-      setSelectedFile(file);
-      if (knownImageTypes.includes(file.type)) {
-        setIsImage(true);
-      }
-      const reader = new FileReader();
-
-      reader.onloadend = () => {
-        setPreview(reader.result);
-      };
-      reader.readAsDataURL(file);
-    };
-    img.src = URL.createObjectURL(file);
-  }, []);
-
-  const artifact = useMemo(async () => {
     return (
-      selectedFile?.type &&
-      preview && (
+      croppedImage && (
         <Artifact
           classNames={{ media: "w-20 h-20", wrapper: "w-fit" }}
           showFooter={false}
@@ -159,8 +134,8 @@ const InscribeBsv21: React.FC<InscribeBsv21Props> = ({ inscribedCallback }) => {
             data: {
               insc: {
                 file: {
-                  type: selectedFile.type,
-                  size: selectedFile.size,
+                  type: "image/png",
+                  size: 0,
                 },
               },
             } as TxoData,
@@ -169,17 +144,35 @@ const InscribeBsv21: React.FC<InscribeBsv21Props> = ({ inscribedCallback }) => {
             txid: "",
             vout: 0,
           }}
-          src={preview as string}
+          src={croppedImage}
           sizes={""}
           latest={true}
         />
       )
     );
-  }, [preview, selectedFile]);
+  }, [croppedImage]);
+
+  const handleIconSelection = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files || !e.target.files[0]) {
+      return;
+    }
+
+    const file = e.target.files[0] as File;
+
+    // No validations here
+    const imageUrl = URL.createObjectURL(file);
+    setSelectedImage(file);
+    setSelectedImageURL(imageUrl);
+    setShowCropper(true); // Open the cropper modal
+
+    if (knownImageTypes.includes(file.type)) {
+      setIsImage(true);
+    }
+  }, []);
 
   const inscribeBsv21 = useCallback(
     async (utxos: Utxo[]) => {
-      if (!ticker || ticker?.length === 0 || selectedFile === null) {
+      if (!ticker || ticker?.length === 0 || (!selectedImage && !croppedImage)) {
         return;
       }
       if (!payPk.value || !ordAddress.value || !fundingAddress.value) {
@@ -190,13 +183,35 @@ const InscribeBsv21: React.FC<InscribeBsv21Props> = ({ inscribedCallback }) => {
 
       const paymentPk = PrivateKey.fromWif(payPk.value);
 
-      // get a buffer of the file
-      const fileData = await selectedFile.arrayBuffer();
+      let contentType: ImageContentType;
+      let dataB64: string;
 
-      console.log({ contentType: selectedFile.type })
+      if (croppedImage) {
+        // Handle Blob URL
+        const response = await fetch(croppedImage);
+        const blob = await response.blob();
+        const arrayBuffer = await blob.arrayBuffer();
+        dataB64 = Buffer.from(arrayBuffer).toString("base64");
+        contentType = blob.type as ImageContentType;
+
+        if (!contentType || !dataB64) {
+          console.error('Invalid cropped image data');
+          setInscribeStatus(FetchStatus.Error);
+          return;
+        }
+      } else if (selectedImage) {
+        const fileData = await selectedImage.arrayBuffer();
+        dataB64 = Buffer.from(fileData).toString("base64");
+        contentType = selectedImage.type as ImageContentType;
+      } else {
+        console.error('No image data available');
+        setInscribeStatus(FetchStatus.Error);
+        return;
+      }
+
       const icon: IconInscription = {
-        dataB64: Buffer.from(fileData).toString("base64"),
-        contentType: selectedFile.type as ImageContentType,
+        dataB64,
+        contentType,
       };
 
       const config: DeployBsv21TokenConfig = {
@@ -217,9 +232,8 @@ const InscribeBsv21: React.FC<InscribeBsv21Props> = ({ inscribedCallback }) => {
 
       const { tx, spentOutpoints, payChange } = await deployBsv21Token(config);
 
-      // TODO: When inscribing new BSV21 the token isnt there yet and causes errors
       setPendingTxs([{
-        returnTo: "/wallet/bsv21", // `/market/bsv21/${tx.id('hex')}_0`,
+        returnTo: "/wallet/bsv21",
         rawTx: tx.toHex(),
         size: tx.toBinary().length,
         fee: tx.getFee(),
@@ -230,11 +244,14 @@ const InscribeBsv21: React.FC<InscribeBsv21Props> = ({ inscribedCallback }) => {
         payChange,
       }]);
       setInscribeStatus(FetchStatus.Success);
+
+      router.push("/preview");
     },
-    [ticker, selectedFile, payPk.value, ordAddress.value, fundingAddress.value, maxSupply, decimals, setPendingTxs],
+    [router, ticker, selectedImage, croppedImage, payPk.value, ordAddress.value, fundingAddress.value, maxSupply, decimals, setPendingTxs],
   );
 
-  const clickInscribe = useCallback(async () => {
+  const clickInscribe = useCallback(async (e: React.FormEvent) => {
+    e.preventDefault();
     if (!payPk.value || !ordAddress.value || !fundingAddress.value) {
       return;
     }
@@ -250,14 +267,15 @@ const InscribeBsv21: React.FC<InscribeBsv21Props> = ({ inscribedCallback }) => {
       inscribeStatus === FetchStatus.Loading ||
       fetchTickerStatus === FetchStatus.Loading ||
       !maxSupply ||
-      (!!selectedFile && !isImage)
+      !croppedImage ||
+      !isImage
     );
   }, [
     ticker?.length,
     inscribeStatus,
     fetchTickerStatus,
     maxSupply,
-    selectedFile,
+    croppedImage,
     isImage,
   ]);
 
@@ -268,8 +286,85 @@ const InscribeBsv21: React.FC<InscribeBsv21Props> = ({ inscribedCallback }) => {
     return calculateIndexingFee(usdRate.value);
   });
 
+  const onCropComplete = useCallback((croppedArea: CropData, croppedAreaPixels: CroppedArea) => {
+    setCroppedAreaPixels(croppedAreaPixels);
+  }, []);
+
+  const handleCropIcon = useCallback(async () => {
+    if (!selectedImage || !croppedAreaPixels || !selectedImageURL) {
+      console.error("No image selected or no crop area", selectedImage, croppedAreaPixels);
+      setShowCropper(false);
+      return;
+    }
+    try {
+      const dataUrl = await getCroppedImg(
+        selectedImageURL,
+        croppedAreaPixels,
+      );
+      if (dataUrl) {
+        setCroppedImage(dataUrl);
+        setSelectedImage(null);
+        setSelectedImageURL(null);
+        setShowCropper(false);
+        setIsImage(true);
+      } else {
+        console.error("Failed to crop image");
+        toast.error("Failed to crop image");
+      }
+    } catch (e) {
+      console.error(e);
+      toast.error("Failed to crop image");
+    }
+  }, [selectedImage, croppedAreaPixels, selectedImageURL]);
+
+  useEffect(() => {
+    console.log({ croppedImage, selectedImage, selectedImageURL, isImage, artifact });
+  }, [croppedImage, selectedImage, selectedImageURL, isImage, artifact]);
   return (
-    <form className="w-full max-w-lg mx-auto" action="/preview">
+    <form className="w-full max-w-lg mx-auto" onSubmit={clickInscribe}>
+      {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+      <dialog
+        id="cropper_modal"
+        className={`modal backdrop-blur ${showCropper ? "modal-open" : ""}`}
+        onClick={() => setShowCropper(false)}
+      >
+        {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+        <div className="modal-box" onClick={(e) => e.stopPropagation()}>
+          <div className="relative w-full h-[400px] bg-[#333]">
+            {selectedImageURL ? <Cropper
+              image={selectedImageURL}
+              crop={crop}
+              zoom={zoom}
+              cropSize={{ width: 400, height: 400 }}
+              objectFit={"contain"}
+              aspect={1}
+              onCropChange={setCrop}
+              onZoomChange={setZoom}
+              onCropComplete={onCropComplete}
+            /> : null}
+          </div>
+          <div className="flex justify-end mt-4">
+            <button
+              type="button"
+              onClick={handleCropIcon}
+              className="mr-2 px-4 py-2 bg-blue-500 text-white rounded"
+            >
+              Crop
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                setShowCropper(false);
+                setSelectedImage(null);
+                setSelectedImageURL(null);
+              }}
+              className="px-4 py-2 bg-gray-500 text-white rounded"
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </dialog>
       <div className="text-white w-full p-2 rounded my-2">Deploy New Token</div>
       <div className="my-2">
         <label className="block mb-4">
@@ -303,7 +398,7 @@ const InscribeBsv21: React.FC<InscribeBsv21Props> = ({ inscribedCallback }) => {
 
       <div className="my-2 flex items-center">
         <div className="w-28 mr-4">
-          {(!selectedFile || !preview) && (
+          {(!croppedImage) && (
             <div className="text-[#555] text-lg">
               <IconWithFallback
                 icon={null}
@@ -312,8 +407,8 @@ const InscribeBsv21: React.FC<InscribeBsv21Props> = ({ inscribedCallback }) => {
               />
             </div>
           )}
-          {selectedFile && preview && isImage && artifact}
-          {selectedFile && !isImage && (
+          {croppedImage && isImage && artifact}
+          {croppedImage && !isImage && (
             <div className="w-full h-full bg-[#111] rounded flex items-center justify-center">
               X
             </div>
@@ -334,7 +429,7 @@ const InscribeBsv21: React.FC<InscribeBsv21Props> = ({ inscribedCallback }) => {
           <input
             type="file"
             className="file-input w-full"
-            onChange={changeFile}
+            onChange={handleIconSelection}
           />
         </label>
       </div>
@@ -382,25 +477,24 @@ const InscribeBsv21: React.FC<InscribeBsv21Props> = ({ inscribedCallback }) => {
         </div>
       )}
       <div className="my-2 flex items-center justify-between mb-4 rounded p-2 text-info-content bg-info">
+        {/* biome-ignore lint/a11y/noLabelWithoutControl: <explanation> */}
         <label className="block w-full">
           BSV21 deployements are indexed immediately. A listing fee of $
           {`${listingFee.value}`} will be required before it shows up in some
           areas on the website. This can be paid later.
         </label>
       </div>
-      {preview && <hr className="my-2 h-2 border-0 bg-[#222]" />}
+      {croppedImage && <hr className="my-2 h-2 border-0 bg-[#222]" />}
 
       <button
         disabled={submitDisabled}
         type="submit"
-        onClick={clickInscribe}
         className="w-full disabled:bg-[#222] disabled:text-[#555] hover:bg-yellow-500 transition bg-yellow-600 enabled:cursor-pointer p-3 text-xl rounded my-4 text-white"
       >
         Preview
       </button>
-    </form>
-  );
-};
+    </form>);
+}
 
 export default InscribeBsv21;
 
