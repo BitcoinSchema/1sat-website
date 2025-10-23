abcbdaa860eb13b0df09f6c4ab90611cf8d46a0b
Luke Rohenaz <rohenaz@gmail.com>
2025-10-22 20:05:06 -0400

    fix: update OpenAI SDK to Vercel AI SDK and update dependencies

--- src/app/api/openai/chat/route.ts
+++ src/app/api/openai/chat/route.ts
@@ -1,21 +1,15 @@
-import { OpenAIStream, StreamingTextResponse } from "ai";
-import OpenAI from "openai";
-
-const openai = new OpenAI({
-	apiKey: process.env.OPENAI_API_KEY,
-});
+import { openai } from "@ai-sdk/openai";
+import { streamText } from "ai";
 
 export const runtime = "edge";
 
 export async function POST(req: Request) {
 	const { messages } = await req.json();
-	const response = await openai.chat.completions.create({
-		model: "gpt-3.5-turbo",
-		stream: true,
+
+	const result = streamText({
+		model: openai("gpt-4o-mini"),
 		messages,
 	});
 
-	const stream = OpenAIStream(response);
-
-	return new StreamingTextResponse(stream);
+	return result.toDataStreamResponse();
 }
