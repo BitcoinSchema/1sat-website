7f77c7d0e630646b8a7f12268681132a60122e65
Satchmo <rohenaz@gmail.com>
2025-11-02 18:35:20 -0500

    Remove Menu component from home page

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -3,14 +3,12 @@ import type React from "react";
 import { Suspense } from "react";
 import { LoaderIcon } from "react-hot-toast";
 import SlideshowLoader from "./loader";
-import Menu from "./menu";
 import FlowLoader from "./flowLoader";
 
 const HomePage: React.FC = async () => {
   return (
     <main className="px-4 flex items-center justify-center w-full min-h-[calc(100dvh-15rem+)]">
       <div className="flex flex-col items-center w-full h-full">
-        <Menu />
         <Suspense
           fallback={
             <div className="w-96 h-fit flex items-center justify-center">
